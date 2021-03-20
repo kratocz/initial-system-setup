31ebcf8bbdc8740d08c290eb2d860a869c28bc94
Petr Kratohvil <krato@krato.cz>
2021-03-20 23:08:17 +0100

    install-desktop-opensuse_Leap_15.sh: zypper install mosh

--- install-desktop-opensuse_Leap_15.sh
+++ install-desktop-opensuse_Leap_15.sh
@@ -19,7 +19,7 @@ test -e "$REPOD/network_cryptocurrencies.repo" || zypper addrepo "https://downlo
 
 zypper refresh
 
-zypper install vim mc screen kdiff3 docker docker-compose opera openvpn ncdu doublecmd-qt5 krusader krusader-doc krename unrar collectd mlocate net-tools-deprecated clamav gcc asbru-cm gimp inkscape dbeaver libQt5WebKit5 libQt5WebKitWidgets5 discord zip chromium apache2-utils npm10 MozillaThunderbird java-13-openjdk duperemove gradle slack lynx links elinks bedup tcpdump code datovka maven php php-composer php-gd php-mbstring php-mysql pinta rclone iotop electrum curlftpfs filezilla
+zypper install vim mc screen kdiff3 docker docker-compose opera openvpn ncdu doublecmd-qt5 krusader krusader-doc krename unrar collectd mlocate net-tools-deprecated clamav gcc asbru-cm gimp inkscape dbeaver libQt5WebKit5 libQt5WebKitWidgets5 discord zip chromium apache2-utils npm10 MozillaThunderbird java-13-openjdk duperemove gradle slack lynx links elinks bedup tcpdump code datovka maven php php-composer php-gd php-mbstring php-mysql pinta rclone iotop electrum curlftpfs filezilla mosh
 
 echo -n "Searching Angular ... " ; which ng || npm install -g @angular/cli
 
