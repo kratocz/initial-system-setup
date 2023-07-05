9b8adfe8ba873a4fe4cfafc2bc186301c2f7feb4
Petr Kratochvil <krato@krato.cz>
2023-07-05 23:01:28 +0200

    install-server-opensuse_Leap-15.2.sh: zypper install tmux

--- install-server-opensuse_Leap-15.2.sh
+++ install-server-opensuse_Leap-15.2.sh
@@ -7,5 +7,5 @@ zypper refresh
 
 #zypper install vim mc gpm rsync sudo compsize docker docker-compose net-tools tree ncdu collectd moreutils openvpn tcpdump lynx links elinks curl net-tools-deprecated php7 composer
 #composer global show | grep -q zaporylie/composer-drupal-optimizations || composer global require zaporylie/composer-drupal-optimizations
-zypper install vim mc gpm rsync sudo compsize docker docker-compose net-tools tree ncdu moreutils openvpn tcpdump lynx links elinks curl net-tools-deprecated iotop rclone mosh fail2ban
+zypper install vim mc gpm rsync sudo compsize docker docker-compose net-tools tree ncdu moreutils openvpn tcpdump lynx links elinks curl net-tools-deprecated iotop rclone mosh fail2ban tmux
 
